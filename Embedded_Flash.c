22b929812c8701dda5f9acfc5590c090b6fc6be5
Mohamed Hafez <muhammedhafez98@gmail.com>
2022-01-27 23:27:42 +0200

    Embedded Flash APIs for Block Programming
    APIs for embedded flash block programming that are used in bootloader application. The block programming API and the wait API must be RAM function for proper operation.

---
+++ Embedded_Flash.c
@@ -0,0 +1,69 @@
+#include "Embedded_Flash.h"
+
+
+uint8_t DataBuffer[2 * FLASH_BLOCK_SIZE];
+uint32_t  AppFlashAddress =  MAIN_USER_RESET_ADDR;
+
+void FLASH_UnlockPROG(void) {
+  FLASH->PUKR = MASS_PROG_KEY_1;
+  FLASH->PUKR = MASS_PROG_KEY_2;
+}
+void FLASH_Lock(FLASH_MemType_TypeDef FLASH_MemType) {
+	FLASH->IAPSR &= (uint8_t)FLASH_MemType;
+}
+
+__ramfunc void FLASH_ProgramBlock(uint16_t BlockNum, FLASH_MemType_TypeDef FLASH_MemType, FLASH_ProgramMode_TypeDef FLASH_ProgMode, uint8_t *Buffer) {
+  uint16_t Count = 0;
+  uint32_t startaddress = 0;
+  
+  if(FLASH_MemType == FLASH_MemType_Program) {
+    startaddress = FLASH_PROGRAM_START_PHYSICAL_ADDRESS;
+  }
+  else {
+    startaddress = FLASH_DATA_EEPROM_START_PHYSICAL_ADDRESS;
+  }
+  
+  /* BlockNum = (FLASH_PROGRAM_START_PHYSICAL_ADDRESS - MAIN_USER_RESET_ADDR)/FLASH_BLOCK_SIZE */
+  startaddress = startaddress + ((uint32_t)BlockNum * FLASH_BLOCK_SIZE);
+  
+  /* Selection of Standard or Fast programming mode */
+  if(FLASH_ProgMode == FLASH_ProgramMode_Standard) {
+    /* Standard programming mode */
+    FLASH->CR2 |= FLASH_CR2_PRG;
+  }
+  else {
+    /* Fast programming mode */
+    FLASH->CR2 |= FLASH_CR2_FPRG;
+  }
+  
+  /* Copy data bytes from RAM to FLASH memory sequentially */
+  for(Count = 0; Count < FLASH_BLOCK_SIZE; Count++) {
+    *((FAR uint8_t*) (uint32_t)startaddress + Count) = ((uint8_t)(Buffer[Count]));
+  }
+}
+
+__ramfunc FLASH_Status_TypeDef FLASH_WaitForLastOperation(FLASH_MemType_TypeDef FLASH_MemType)
+{
+  uint16_t timeout = OPERATION_TIMEOUT;
+  uint8_t flagstatus = 0x00;
+  
+  /* Wait until operation completion or write protected page occurred */
+  if(FLASH_MemType == FLASH_MemType_Program) {
+    while((flagstatus == 0x00) && (timeout != 0x00)) {
+      flagstatus = (uint8_t)(FLASH->IAPSR & (uint8_t)(FLASH_IAPSR_EOP | FLASH_IAPSR_WR_PG_DIS));
+      timeout--;
+    }
+  }
+  else {
+    while((flagstatus == 0x00) && (timeout != 0x00)) {
+      flagstatus = (uint8_t)(FLASH->IAPSR & (uint8_t)(FLASH_IAPSR_HVOFF | FLASH_IAPSR_WR_PG_DIS));
+      timeout--;
+    }
+  }
+  
+  if(timeout == 0x00) {
+    flagstatus = FLASH_Status_TimeOut;
+  }
+  
+  return((FLASH_Status_TypeDef)flagstatus);
+}
---
+++ Embedded_Flash.h
@@ -0,0 +1,53 @@
+/*
+ * Embedded_Flash.h
+ *
+ *  Created on: Jan 27, 2022
+ *      Author: Mohamed.Hafez
+*/
+
+#ifndef		EMBEDDED_FLASH_H
+#define		EMBEDDED_FLASH_H
+
+#include "stm8l15x.h"
+#include "stdint.h"
+
+#define FLASH_BLOCK_SIZE                           ((uint8_t)0x80)        /*!< Number of bytes in a Block*/
+#define FLASH_PROGRAM_START_PHYSICAL_ADDRESS       ((uint32_t)0x00008000) /*!< Flash: start address */  
+#define FLASH_PROGRAM_END_PHYSICAL_ADDRESS         ((uint32_t)0x00017FFF) /*!< Flash: end address */
+#define MAIN_USER_RESET_ADDR 0x009400
+#define OPERATION_TIMEOUT					 	   ((uint16_t)0xFFFF)
+#define MASS_PROG_KEY_1							   (0x56)
+#define MASS_PROG_KEY_2							   (0xAE)
+
+
+typedef enum {
+  FLASH_FLAG_HVOFF     = (uint8_t)0x40,     /*!< End of high voltage flag */
+  FLASH_FLAG_DUL       = (uint8_t)0x08,     /*!< Data EEPROM unlocked flag */
+  FLASH_FLAG_EOP       = (uint8_t)0x04,     /*!< End of programming (write or erase operation) flag */
+  FLASH_FLAG_PUL       = (uint8_t)0x02,     /*!< Flash Program memory unlocked flag */
+  FLASH_FLAG_WR_PG_DIS = (uint8_t)0x01      /*!< Write attempted to protected page flag */
+}FLASH_FLAG_TypeDef;
+
+typedef enum {
+  FLASH_ProgramMode_Standard = (uint8_t)0x00, /*!< Standard programming mode */
+  FLASH_ProgramMode_Fast     = (uint8_t)0x10  /*!< Fast programming mode */
+}FLASH_ProgramMode_TypeDef;
+
+typedef enum {
+  FLASH_Status_Write_Protection_Error = (uint8_t)0x01, /*!< Write attempted to protected Block */
+  FLASH_Status_TimeOut                = (uint8_t)0x02, /*!< Time out error */
+  FLASH_Status_Successful_Operation   = (uint8_t)0x04  /*!< End of operation flag */
+}FLASH_Status_TypeDef;
+
+typedef enum {
+ FLASH_MemType_Program    = (uint8_t)0xFD, /*!< Program memory */
+ FLASH_MemType_Data      = (uint8_t)0xF7  /*!< Data EEPROM memory */
+}FLASH_MemType_TypeDef;
+
+void FLASH_UnlockPROG(void);
+void FLASH_Lock(FLASH_MemType_TypeDef FLASH_MemType);
+__ramfunc void FLASH_ProgramBlock(uint16_t BlockNum, FLASH_MemType_TypeDef FLASH_MemType, FLASH_ProgramMode_TypeDef FLASH_ProgMode, uint8_t *Buffer);
+__ramfunc FLASH_Status_TypeDef FLASH_WaitForLastOperation(FLASH_MemType_TypeDef FLASH_MemType);
+
+
+#endif //EMBEDDED_FLASH_H
